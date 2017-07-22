18e07b88da28319e2840721309dfa26ffa427437
sunzz <sunzz@hytch.com>
2017-07-22 09:40:28 +0800

    web应用程序域

---
+++ Intelligencer/Intelligencer1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+
+namespace Intelligencer
+{
+    public class Intelligencer1 : MarshalByRefObject
+    {
+        public string Report()
+        {
+            AppDomain domain = AppDomain.CurrentDomain;
+
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendFormat("Domain ID:{0}\r\n", domain.Id);
+
+            builder.AppendFormat("VirtualPath:{0}\r\n", HostingEnvironment.ApplicationVirtualPath);
+
+            builder.AppendFormat("PhysicalPath:{0}\r\n", HostingEnvironment.ApplicationPhysicalPath);
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ Web应用程序域Controll/Program.cs
@@ -0,0 +1,26 @@
+﻿using Intelligencer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+
+namespace Web应用程序域Controll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type hostType = typeof(Intelligencer1);
+
+            Intelligencer1 intelligencer = ApplicationHost.CreateApplicationHost(hostType, "/", Environment.CurrentDirectory) as Intelligencer1;
+
+            Console.WriteLine("Current Domain ID:{0}\r\n", AppDomain.CurrentDomain.Id);
+
+            Console.WriteLine(intelligencer.Report());
+
+            Console.ReadKey();
+        }
+    }
+}
